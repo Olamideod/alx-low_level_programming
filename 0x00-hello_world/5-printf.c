bc8c6cee326f96948c211601090324df718351de
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-15 20:31:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of every program
+ *
+ * Return: Always written in 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
