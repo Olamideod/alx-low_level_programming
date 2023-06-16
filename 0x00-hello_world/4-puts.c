641048230d97432d13e18e3e8d47edc0d402ccff
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-15 20:15:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of every program
+ *
+ * Return: Always written in 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
