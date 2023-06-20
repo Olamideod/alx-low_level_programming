9d6e2ff84ea982d253369286ec765437d2823160
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-19 20:24:22 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,38 +1,21 @@
-
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-* main - starting point
+* main - stating point
 *
-* Description: print difference
-* combinations more than one digits.
+* Description: printing of alphabet
 *
 * Return: Success
 */
 
 int main(void)
 {
-int digit1 = 0, digit2;
+char ch = 'a';
 
-while (digit1 <= 9)
+while (ch <= 'z')
 {
-digit2 = 0;
-while (digit2 <= 9)
-{
-if (digit1 != digit2 && digit1 < digit2)
-{
-putchar(digit1 + 48);
-putchar(digit2 + 48);
-
-if (digit1 + digit2 != 17)
-{
-putchar(',');
-putchar(' ');
-}
-}
-++digit2;
-}
-++digit1;
+putchar(ch);
+++ch;
 }
 putchar('\n');
 
