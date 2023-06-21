d5b09795459244559775713b8c761d1e9d48413a
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-20 21:42:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* main - starting point
+*
+* Description: A program that prints _putchar, followed by a new line
+*
+* Return: Success
+*/
+
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+
+return (0);
+}
+
