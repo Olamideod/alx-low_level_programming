621c8816be32672ef2dcb554b6f58db3bb5d19d4
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-21 17:07:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+
+#include"main.h"
+
+/**
+* add - add both integers
+*
+* _a: first input
+* _b: second input
+*
+* Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+int sum = a + b;
+
+return (sum);
+}
+
+
