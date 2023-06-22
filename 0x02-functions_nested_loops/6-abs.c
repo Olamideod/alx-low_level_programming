5b4c3d929e4c6836150fb7525a72a62b040050de
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-21 19:10:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* Main: Start point
+* Description - returns absolute value of an int
+* from which to get an absolute value
+* Return: Void.
+*/
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
+
