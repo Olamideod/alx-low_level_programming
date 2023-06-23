00e2a71a60f105256a7044ab524b13465a8002db
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-22 17:40:41 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* main - check the code.
-* 
-* Return: Always 0
+* _isupper - search for uppercase character
+* @c: the character to be searched
+* Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
