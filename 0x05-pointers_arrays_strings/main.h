b00a7f567a7164ee1dcfd0b32343611d8eef45a4
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-27 09:39:03 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,29 +1,17 @@
-#include <main.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 2.
- * On error, -2 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#endif
