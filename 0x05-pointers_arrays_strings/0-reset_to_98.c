1ac58dc3fcaf263898a5f981527a430974285e7c
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-26 18:08:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+0-reset_to_98.c
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Olamide_code
+*  Return: success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
