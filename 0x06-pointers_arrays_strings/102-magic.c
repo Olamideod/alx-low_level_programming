7b589d24c98bd64eed5c01a4391e69672f42f8b5
Olamideod <oluwamusiwaolamide@gmail.com>
2023-06-30 18:53:35 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,14 @@
-#include <stdio.h>
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _checkcode - this code, so that the program prints a[2] = 98,
+ * followed by a new line.
+ * you are not allowed to use the variable a
+ * in your new line of code
+ * You are not allowed to modify the variable p
+ * Return: code to be checked
+ */
 
 int main(void)
 {
@@ -8,16 +18,5 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+putchar("a[2] = %d\n", a[2]);
 }
