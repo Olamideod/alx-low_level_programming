6241d0602d1d2e585264551150bc86428e7e3716
Olamideod <oluwamusiwaolamide@gmail.com>
2023-07-28 12:57:16 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,22 +14,22 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+        size_t count = 0;
 
-	/* while we have a pointer to list */
-	while (h != NULL)
-	{
-		/* dereference pointer and check if str is null */
-		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else /* if not NULL */
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
-		count++;
-	}
-	return (count);
+        /* while we have a pointer to list */
+        while (h != NULL)
+        {
+                /* dereference pointer and check if str is null */
+                if (h->str == NULL)
+                {
+                        printf("[%d] %s\n", 0, "(nil)");
+                }
+                else /* if not NULL */
+                {
+                        printf("[%d] %s\n", h->len, h->str);
+                }
+                h = h->next;
+                count++;
+        }
+        return (count);
 }
