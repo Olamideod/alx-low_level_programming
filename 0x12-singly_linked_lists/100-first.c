ee92f57aca6e110668e929d5d556e3709790b88f
Olamideod <oluwamusiwaolamide@gmail.com>
2023-07-28 13:45:53 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+ * before_main - Funcion to print a specific str before the main,
+ * function to be executed.
+ *
+ * Return: return 0.
+ */
+
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
