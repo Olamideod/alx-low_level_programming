5f798c56332bb2af5217b65d472c770a95d02590
root <root@DESKTOP-U661651.localdomain>
2023-10-11 03:04:32 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_table_create - Creating  hash table.
+ * @size: The size of the arry,
  *
  * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ *         Otherwise - A pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
